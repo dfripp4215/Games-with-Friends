fd519fecf8274a7d8803b245feb0e854b5d27b11
mattvioli <mattvioli@gmail.com>
2021-08-01 16:03:14 +1000

    storage update

--- client/js/storage.js
+++ client/js/storage.js
@@ -6,6 +6,7 @@ Storage.prototype.getObject = function(key) {
   return JSON.parse(this.getItem(key));
 }
 
+localStorage.setObject('userData', {user: {name: '' , email: '', loggedIn: false}})
+
 const userData = localStorage.getObject('userData');
 
-localStorage.setObject('userData', {user: {name: '' , email: '', loggedIn: false}})
