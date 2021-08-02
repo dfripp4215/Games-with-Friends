954ac0add74378b2f18a1521dde9be02194e22c1
Dustin <dustinfripp@gmail.com>
2021-08-02 21:04:30 +1000

    validate_events middleware added

---
+++ middlewares/events/validate_events.js
@@ -0,0 +1,17 @@
+const isBlank = require("../tools/is_blank");
+const validationError = require("../tools/validation_error");
+
+function validateEvent(req, res, next) {
+    const date = req.body.date
+    const friends = req.body.friends
+
+    if (isBlank(date)) {
+        throw validationError("Date is required")
+
+    } else if (isBlank(friends)) {   
+        throw validationError("Must choose at least 1 friend")
+    }
+    next()
+}
+
+module.exports = validateTreasure
