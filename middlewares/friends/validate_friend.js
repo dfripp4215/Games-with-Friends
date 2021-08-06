65a90fc5ec43bc7afd9cecdbada2ad3abc481d13
Dustin <dustinfripp@gmail.com>
2021-08-06 10:38:04 +1000

    friends validation updated

--- middlewares/friends/validate_friend.js
+++ middlewares/friends/validate_friend.js
@@ -4,11 +4,14 @@ const findFriend = require("../tools/friend_finder")
 
 function validateFriend(req, res, next) {
     const friendEmail = req.body.friendEmail
+    const userEmail = req.body.userEmail
 
     if (isBlank(friendEmail)) {
         throw validationError("Users Email is required")
     } else if (findFriend(friendEmail)) {
         throw validationError("User not found")
+    } else if (userEmail === '') {
+        throw validationError("Please login to add friends")
     }
 
     next()
